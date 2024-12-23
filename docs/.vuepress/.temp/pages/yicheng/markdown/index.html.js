ef8c3a4cc04d9f94c45c15341283f2be1f4360f9
lotoze <lotoze@163.com>
2024-12-23 22:24:25 +0800

    [update] 奕澄Markdown审核不通过隐私协议修改

--- docs/.vuepress/.temp/pages/yicheng/markdown/index.html.js
+++ docs/.vuepress/.temp/pages/yicheng/markdown/index.html.js
@@ -1,5 +1,5 @@
 import comp from "F:/projects/Product/lotoze-blog-vuepress/docs/.vuepress/.temp/pages/yicheng/markdown/index.html.vue"
-const data = JSON.parse("{\"path\":\"/yicheng/markdown/\",\"title\":\"奕澄Markdown\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"奕澄Markdown\",\"heroImage\":\"/images/yicheng-markdown-logo.png\",\"description\":\"222222~\",\"footer\":\"MIT Licensed | Copyright © 2024-present lotoze Community\"},\"headers\":[],\"git\":{},\"filePathRelative\":\"yicheng/markdown/README.md\",\"excerpt\":\"\"}")
+const data = JSON.parse("{\"path\":\"/yicheng/markdown/\",\"title\":\"奕澄Markdown\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"奕澄Markdown\",\"heroImage\":\"/images/yicheng-markdown-logo.png\",\"description\":\"222222~\",\"footer\":\"MIT Licensed | Copyright © 2024-present lotoze Community\"},\"headers\":[],\"git\":{\"updatedTime\":1733818731000,\"contributors\":[{\"name\":\"lotoze\",\"email\":\"lotoze@163.com\",\"commits\":1,\"url\":\"https://github.com/lotoze\"}]},\"filePathRelative\":\"yicheng/markdown/README.md\",\"excerpt\":\"\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
